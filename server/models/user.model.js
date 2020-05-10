90309dad5e70ff301df480e76ba54e3727c6fbaf
Elvira Ramírez <elvirarp92@gmail.com>
2020-05-10 20:17:21 +0200

    create User model

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -1,13 +1,30 @@
-const mongoose = require("mongoose")
+const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
-const userSchema = new Schema({
-    username: String,
-    password: String
-}, {
-    timestamps: true
-})
+const userSchema = new Schema(
+  {
+    username: { type: String, required: true },
+    password: { type: String, required: true },
+    email: { type: String, required: true },
+    status: {
+      type: String,
+      enum: ['active', 'pending confirmation'],
+      default: 'pending confirmation',
+    },
+    confirmationCode: String,
+    role: {
+      type: String,
+      enum: ['client', 'professional'],
+      default: 'client',
+    },
+    businesses: { type: [Schema.Types.ObjectId], ref: 'Salon' },
+    appointments: { type: [Schema.Types.ObjectId], ref: 'Appointment' },
+  },
+  {
+    timestamps: true,
+  }
+)
 
-const User = mongoose.model("User", userSchema)
+const User = mongoose.model('User', userSchema)
 
 module.exports = User
