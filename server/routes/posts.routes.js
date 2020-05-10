56c5956faf52b21d7bead65b50163fea73ee1032
Elvira Ramírez <elvirarp92@gmail.com>
2020-05-10 18:44:29 +0200

    create route for specific posts

--- server/routes/posts.routes.js
+++ server/routes/posts.routes.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const router = express.Router
 
-
 //READ
 router.get('/getsalonposts/:salonId', (req, res, next) => {
+  /*WIP*/
+})
+
+router.get('/getpost/:id', (req, res, next) => {
   /*WIP*/
 })
 
