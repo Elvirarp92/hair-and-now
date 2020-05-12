127668180f607725e936f5c29cb365595ac1c0f1
Elvira Ramírez <elvirarp92@gmail.com>
2020-05-12 18:09:07 +0200

    signup route ready in API, pending testing

--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -1,10 +1,103 @@
 const express = require('express')
 const router = express.Router
 const passport = require('passport')
+const crypto = require('crypto')
+const nodemailer = require('nodemailer')
+
+import { v4 as uuidv4 } from 'uuid'
+
+const User = require('./../models/user.model')
+
+let transporter = nodemailer.createTransport({
+  service: 'Gmail',
+  auth: {
+    user: process.env.EMAIL,
+    password: process.env.PASSWORD,
+  },
+})
 
 router.post('/signup', (req, res, next) => {
-  /*WIP*/
+  const username = req.body.username
+  const password = req.body.password
+  const email = req.body.email
+  const role = req.body.role
+
+  if (!username || !password || !email) {
+    res.status(400).json({ message: 'Provide username, password and email' })
+    return
+  }
+
+  if (password.length < 8) {
+    res.status(400).json({
+      message:
+        'Please make your password at least 8 characters long for security purposes.',
+    })
+    return
+  }
+
+  User.findOne({ username }, (err, foundUser) => {
+    if (err) {
+      res.status(500).json({ message: 'Username check went bad.' })
+      return
+    }
+
+    if (foundUser) {
+      res.status(400).json({ message: 'Username taken. Choose another one.' })
+      return
+    }
+  })
+
+  User.findOne({ email }, (err, foundUser) => {
+    if (err) {
+      res.status(500).json({ message: 'Email check went bad.' })
+      return
+    }
+
+    if (foundUser) {
+      res.status(400).json({ message: 'Email taken. Choose another one.' })
+      return
+    }
+  })
+
+  const hmac = crypto.createHmac('sha512', process.env.CRYPTOKEY1)
+  hmac.update(password)
+
+  //PENDING NODEMAILER
+
+  User.create({
+    username: username,
+    password: hmac.digest('base64'),
+    keyId: 1,
+    email: email,
+    role: role,
+    confirmationCode: uuidv4(),
+    status: 'pending confirmation',
+  })
+    .then((user) => {
+      if (user.role == 'client') {
+        transporter.sendMail({
+          from: process.env.EMAIL,
+          to: user.email,
+          subject: 'Confirma tu registro a Hair&Now ☑️',
+          text: `Para poder acceder a Hair&Now con tu usuario y contraseña, 
+      por favor, confirma tu registro en el siguiente 
+      enlace: http://localhost:5000/api/confirm/${user.confirmationCode}`,
+          html: `<p>Para poder acceder a LabExpress con tu usuario y contraseña, 
+      por favor, confirma tu registro en 
+      <a href="http://localhost:5000/api/confirm/${user.confirmationCode}">
+      el siguiente enlace.</a></p>`,
+        })
+      }
+    })
+    .then((user) => {
+      res.status(200).json(user)
+    })
+    .catch((err) => {
+      res.status(400).json({ message: 'Saving user to database went wrong.' })
+    })
 })
+
+//User confirmation
 
 router.post('/login', (req, res, next) => {
   /*WIP*/
