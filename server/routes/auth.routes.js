9788d1f4f15cd856df8bcdc799f92c6544470554
Elvira Ramírez <elvirarp92@gmail.com>
2020-05-12 18:16:45 +0200

    confirmation route ready

--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -98,6 +98,21 @@ router.post('/signup', (req, res, next) => {
 })
 
 //User confirmation
+router.get('/confirm/:confirmCode', (req, res, next) => {
+  User.findOneAndUpdate(
+    { confirmationCode: req.params.confirmationCode },
+    { status: 'active' }
+  )
+    .then((user) => {
+      req.login(user)
+    })
+    .then((user) => {
+      res.status(200).json(user)
+    })
+    .catch((err) => {
+      res.status(500).json({ message: 'Login after signup went bad.' })
+    })
+})
 
 router.post('/login', (req, res, next) => {
   /*WIP*/
