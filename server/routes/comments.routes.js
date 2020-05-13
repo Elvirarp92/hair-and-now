c9b622d9a22763e7633299323826898bc4c9ddc0
Elvira Ramírez <elvirarp92@gmail.com>
2020-05-13 14:06:35 +0200

    routes for getting comments by salon and post ready

--- server/routes/comments.routes.js
+++ server/routes/comments.routes.js
@@ -1,10 +1,29 @@
 const express = require('express')
 const router = express.Router()
 
+const Salon = require('./../models/salon.model')
+const Post = require('./../models/post.model')
+const Comment = require('./../models/comment.model')
 
 //READ
 router.get('/getsaloncomments/:salonId', (req, res, next) => {
-  /*WIP*/
+  Salon.findById(req.params.salonId)
+    .then((salon) => {
+      res.json(salon.comments)
+    })
+    .catch((err) => {
+      next(new Error(err))
+    })
+})
+
+router.get('/getpostcomments/:postId', (req, res, next) => {
+  Post.findById(req.params.postId)
+    .then((post) => {
+      res.json(post.comments)
+    })
+    .catch((err) => {
+      next(new Error(err))
+    })
 })
 
 //CREATE
