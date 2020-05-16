2f95a426912b43043225afa678510e796ec98b0d
Elvira Ramírez <elvirarp92@gmail.com>
2020-05-16 16:34:06 +0200

    fix cors middleware config

--- server/configs/middleware.config.js
+++ server/configs/middleware.config.js
@@ -1,10 +1,22 @@
 const bodyParser = require('body-parser')
 const cookieParser = require('cookie-parser')
 const logger = require('morgan')
+const cors = require('cors')
 
-module.exports = app => {
-    app.use(logger('dev'))
-    app.use(bodyParser.json())
-    app.use(bodyParser.urlencoded({ extended: false }))
-    app.use(cookieParser())
+// CORS setup
+const whitelist = `${process.env.DOMAIN}`
+const corsOptions = {
+  origin: (origin, cb) => {
+    const originIsWhitelisted = whitelist.includes(origin)
+    cb(null, originIsWhitelisted)
+  },
+  credentials: true,
+}
+
+module.exports = (app) => {
+  app.use(cors(corsOptions))
+  app.use(logger('dev'))
+  app.use(bodyParser.json())
+  app.use(bodyParser.urlencoded({ extended: false }))
+  app.use(cookieParser())
 }
