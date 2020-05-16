9b8451d300dd75b17fb6e0b0fcc023fe16751811
Elvira Ramírez <elvirarp92@gmail.com>
2020-05-16 21:26:07 +0200

    add navbar

---
+++ client/src/components/ui/lightNavbar/lightNavbar.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+import './lightNavbar.css'
+
+import Navbar from 'react-bootstrap/Navbar'
+import Nav from 'react-bootstrap/Nav'
+
+const LightNavbar = () => {
+  return (
+    <Navbar bg="light" expand="lg">
+      <Navbar.Brand href="/" bsPrefix="brand">
+        Hair & Now
+      </Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="mr-auto">
+          <Nav.Link href="/">Inicio</Nav.Link>
+          <Nav.Link href="#">Reservas</Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  )
+}
+
+export default LightNavbar
