9ab49f393d1c304b0bf0bf1d2f073231bafd051a
Elvira Ramírez <elvirarp92@gmail.com>
2020-05-16 23:20:39 +0200

    add schedule accordion menu

--- client/src/components/pages/salonDetails/salonDetails.js
+++ client/src/components/pages/salonDetails/salonDetails.js
@@ -5,6 +5,8 @@ import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 import Button from 'react-bootstrap/Button'
+import Accordion from 'react-bootstrap/Accordion'
+import Card from 'react-bootstrap/Card'
 
 import LightNavbar from './../../ui/lightNavbar/lightNavbar'
 
@@ -36,22 +38,82 @@ class SalonDetails extends Component {
 
   render() {
     return (
-      <Container as="main">
+      <main>
         <LightNavbar />
-        <h1>{this.state.name}</h1>
-        <Row>
-          <Col>
-            <p className="salon-info-tidbits">{this.state.type}</p>
-          </Col>
-          <Col>
-            <p className="rating-module">{this.state.rating}/10</p>
-          </Col>
-        </Row>
-        <Button size="lg" block="true" className="red-button">
-          Pedir cita
-        </Button>
-        {/* <p>{`${this.state.address.street},`}</p> */}
-      </Container>
+        <Container>
+          <h1>{this.state.name}</h1>
+          <Row>
+            <Col>
+              <p className="salon-info-tidbits">{this.state.type}</p>
+            </Col>
+            <Col>
+              <p className="rating-module">{this.state.rating}/10</p>
+            </Col>
+          </Row>
+          {/* placeholder */}
+          <img
+            src="https://images.unsplash.com/photo-1534778356534-d3d45b6df1da?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80"
+            alt={this.state.name}
+          />
+          {this.state.address && (
+            <p>{`${this.state.address.street}, ${this.state.address.number}, ${this.state.address.zipcode} ${this.state.address.town} (${this.state.address.province})`}</p>
+          )}
+          <Button size="lg" block="true" className="red-button">
+            Pedir cita
+          </Button>
+          <Accordion className="accordion-menu">
+            <Card>
+              <Accordion.Toggle as={Card.Header} eventKey="0">
+                Horario
+              </Accordion.Toggle>
+              <Accordion.Collapse eventKey="0">
+                <Card.Body>
+                  {this.state.schedule && (
+                    <div>
+                      <p>
+                        Lunes: {this.state.schedule.monday.openingTime} / 
+                        {this.state.schedule.monday.closingTime}
+                      </p>
+                      <p>
+                        Martes: {this.state.schedule.tuesday.openingTime} / 
+                         {this.state.schedule.tuesday.closingTime}
+                      </p>
+                      <p>
+                        Miércoles: {this.state.schedule.wednesday.openingTime} / 
+                         {this.state.schedule.wednesday.closingTime}
+                      </p>
+                      <p>
+                        Jueves: {this.state.schedule.thursday.openingTime} / 
+                         {this.state.schedule.thursday.closingTime}
+                      </p>
+                      <p>
+                        Viernes: {this.state.schedule.friday.openingTime} / 
+                         {this.state.schedule.friday.closingTime}
+                      </p>
+                      <p>
+                        Sábado: {this.state.schedule.saturday.openingTime} / 
+                         {this.state.schedule.saturday.closingTime}
+                      </p>
+                      <p>
+                        Domingo: {this.state.schedule.sunday.openingTime} / 
+                         {this.state.schedule.sunday.closingTime}
+                      </p>
+                    </div>
+                  )}
+                </Card.Body>
+              </Accordion.Collapse>
+            </Card>
+            <Card>
+              <Accordion.Toggle as={Card.Header} eventKey="1">
+                Comentarios
+              </Accordion.Toggle>
+              <Accordion.Collapse eventKey="1">
+                <Card.Body>Hello! I'm another body</Card.Body>
+              </Accordion.Collapse>
+            </Card>
+          </Accordion>
+        </Container>
+      </main>
     )
   }
 }
