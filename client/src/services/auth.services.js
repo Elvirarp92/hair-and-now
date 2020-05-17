1dde1bad284f3a2ce04103e353814b5e79092844
Elvira Ramírez <elvirarp92@gmail.com>
2020-05-17 21:02:02 +0200

    create auth services

---
+++ client/src/services/auth.services.js
@@ -0,0 +1,13 @@
+import axios from 'axios'
+
+export default class services {
+  constructor() {
+    this.service = axios.create({
+      baseURL: 'http://localhost:5000/api', //or http://hair-and-now.herokuapp.com/api
+      withCredentials: true,
+    })
+  }
+
+  signup = ({ username, password, email, role }) =>
+    this.service.post('/signup', { username, password, email, role })
+}
