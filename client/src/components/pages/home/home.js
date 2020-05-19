8418b4578d94f1724d93f0f3ec496b91ca84d839
Elvira Ramírez <elvirarp92@gmail.com>
2020-05-19 17:43:16 +0200

    add link to register button on homepage

--- client/src/components/pages/home/home.js
+++ client/src/components/pages/home/home.js
@@ -7,6 +7,8 @@ import Button from 'react-bootstrap/Button'
 import Accordion from 'react-bootstrap/Accordion'
 import Card from 'react-bootstrap/Card'
 
+import { Link } from 'react-router-dom'
+
 import SalonList from './salonList/salonList'
 
 import SalonService from './../../../services/salons.services'
@@ -33,32 +35,34 @@ class Home extends Component {
 
   render() {
     return (
-      <Container as="main">
-        <Row as="section">
+      <Container as='main'>
+        <Row as='section'>
           <Col xs={12} s={12}>
             <h1>Hair & Now</h1>
           </Col>
-          <Col xs={12} s={12} m={6} className="is-flex is-centered">
-            <SalonList salons={this.state.salons}/>
+          <Col xs={12} s={12} m={6} className='is-flex is-centered'>
+            <SalonList salons={this.state.salons} />
           </Col>
           <Col xs={12} s={12} m={6}>
             <Accordion>
               <Card>
-                <Accordion.Toggle as={Card.Header} eventKey="0">
+                <Accordion.Toggle as={Card.Header} eventKey='0'>
                   Búsqueda avanzada
                 </Accordion.Toggle>
-                <Accordion.Collapse eventKey="0">
+                <Accordion.Collapse eventKey='0'>
                   <Card.Body>Hello! I'm the body</Card.Body>
                 </Accordion.Collapse>
               </Card>
             </Accordion>
-            <Button size="lg" block="true" className="red-button">
+            <Button size='lg' block='true' className='red-button'>
               Buscar
             </Button>
-            <p className="pro-cta">¿Profesional? Anúnciate con nosotros</p>
-            <Button size="lg" block="true" className="red-button">
-              Regístrate
-            </Button>
+            <p className='pro-cta'>¿Profesional? Anúnciate con nosotros</p>
+            <Link to='/signup'>
+              <Button size='lg' block='true' className='red-button'>
+                Regístrate
+              </Button>
+            </Link>
           </Col>
         </Row>
       </Container>
